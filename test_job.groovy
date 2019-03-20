a08e219d80b55bc8d5cc4c687aae7467ad82c7cb
Arthur <a.verdier@outlook.fr>
2019-03-20 22:22:02 +0100

    Update groovy

--- test_job.groovy
+++ test_job.groovy
@@ -10,7 +10,7 @@ job('Test PHP Groovy job') {
     }
     steps {
         shell("docker container create --name dummygroovy -v tempvolumegroovy:/app hello-world")
-        shell('docker cp "$(PWD)/." dummygroovy:/app')
+        shell('docker cp "$(pwd)/." dummygroovy:/app')
         shell("docker rm dummygroovy")
         shell("docker run -v tempvolumegroovy:/app --rm phpunit/phpunit:latest --bootstrap ExempleClass.php ExempleTest.php")
         shell("docker volume rm tempvolumegroovy")
