e8178176257f3011844d78a4e34edfc4a7c5b1cc
Arthur <a.verdier@outlook.fr>
2019-03-20 22:14:06 +0100

    Update groovy

---
+++ test_log_pwd.groovy
@@ -0,0 +1,14 @@
+job('Test PWD Groovy job') {
+    scm {
+        git('https://github.com/averdier/enigma_jenkins_php') {  node -> 
+            node / gitConfigName('rastadev')
+            node / gitConfigEmail('arthur@elonet.fr')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    steps {
+        shell('ls "$(PWD)"')
+    }
+}
